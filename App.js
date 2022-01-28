2ea1d04cfaca565c356df827be03273f8d006bf0
Ramon Morban <morbanjunior@gmail.com>
2022-01-28 11:34:51 -0400

    Error solution

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function HomeScreenA() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen A</Text>
+    </View>
+  );
+}
+
+function HomeScreenB() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen B</Text>
+    </View>
+  );
+}
+
+function HomeScreenC() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen C</Text>
+    </View>
+  );
+}
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeScreenA} />
+      <Drawer.Screen name="Services" component={HomeScreenB} />
+      <Drawer.Screen name="Store" component={HomeScreenC} />
+    </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
